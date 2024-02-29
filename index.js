291586b7fb342dba9c9ce2f43e015ec4699cbbd2
k4myk-glitch <153441961+k4myk-glitch@users.noreply.github.com>
2024-02-29 16:59:24 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["W grze Three Hawkers Boys"];
+const statusMessages = ["W grze Three Hawkers Boys", "przygotowuje jedzenie"];
 
 
 let currentIndex = 0;
